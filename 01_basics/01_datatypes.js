917e8d2a3b7880a7d7a00e298b64cc5b994832b8
Subhajyoti Nath <pubgguyarko6289@gmail.com>
2025-02-09 23:16:56 +0530

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//  alert(3 + 3)/ // we are using nodejs, not browser
+
+console.log(3 + 3)
+                     // code readability should be high
+console.log("Arko")
+
+
+let name = "arko"
+let age = 18
+let isLoggedIn = false 
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => unassigned value 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined);
